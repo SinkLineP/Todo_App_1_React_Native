10ce9d1cf56697ea6c5467259b72293e4367a9f7
SinkLineP <popovartem505@gmail.com>
2023-03-02 15:05:52 +0300

    created button delete todo

--- components/TodoItem/TodoItem.js
+++ components/TodoItem/TodoItem.js
@@ -1,16 +1,16 @@
 import React from "react";
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
-
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TodoItem({ item, handleClick }) {
   const { text, key } = item;
-  // console.log(text + " in component!");
 
 
 
   return (
-    <TouchableOpacity onPress={() => handleClick(key)}>
-      <Text style={styles.item}>{text}</Text>
+    <TouchableOpacity style={styles.item}>
+      <Text>{text}</Text>
+      <MaterialIcons name="delete-forever" size={24} color="red" onPress={() => handleClick(key)}/>
     </TouchableOpacity>
   )
 }
@@ -23,6 +23,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
   }
 })
 
