fc4bae0dc09ac7820bd378ee9b4b9498a9597402
SinkLineP <popovartem505@gmail.com>
2023-03-03 08:01:57 +0300

    finish

--- components/TodoItem/TodoItem.js
+++ components/TodoItem/TodoItem.js
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
   item: {
     padding: 16,
     marginTop: 16,
-    borderColor: "#bbb",
+    borderColor: "coral",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
